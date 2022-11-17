fa9b36ca8cd58187c701507f65ff9151b7e3d9b1
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-17 04:21:09 +0100

    done with the department

---
+++ src/main/java/project/userservice/VO/Department.java
@@ -0,0 +1,19 @@
+package project.userservice.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    private Long departmentId;
+
+    private String name;
+
+    private String address;
+
+    private String code;
+}
