5d5967f7abb3b1a93445e0eff487b3ec64f90c10
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-17 04:36:52 +0100

    done with the service vo

--- src/main/java/project/userservice/UserServiceApplication.java
+++ src/main/java/project/userservice/UserServiceApplication.java
@@ -1,15 +1,18 @@
 package project.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+        log.info("Finished running application");
     }
 
     @Bean
