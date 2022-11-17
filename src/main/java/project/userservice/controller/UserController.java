992b138a9cd3e900e1215609f729c57be5ea1507
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-17 04:25:48 +0100

    done with the controller

--- src/main/java/project/userservice/controller/UserController.java
+++ src/main/java/project/userservice/controller/UserController.java
@@ -1,10 +1,8 @@
 package project.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.userservice.VO.ResponseTemplateVO;
 import project.userservice.entity.User;
 import project.userservice.service.UserService;
 
@@ -18,5 +16,10 @@ public class UserController {
     @PostMapping("/")
     public User saveUser(@RequestBody User user){
         return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
+        return userService.getUserWithDepartment(userId);
     }
 }
